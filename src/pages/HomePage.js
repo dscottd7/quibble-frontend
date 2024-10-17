bdce9ca194b7acba9447edac8dc567ef03bad44d
MaiEdward <edwmai1813@gmail.com>
2024-10-17 12:47:38 -0700

    Product URL Form
    Created a form for users to submit the two URLs of products for comparison.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react';
+
 
 const HomePage = () => {
+
+  // set state for urls
+  const [urls, setUrls] = useState({
+    url1: '',
+    url2: ''
+  });
+
+  // change states for urls
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setUrls((prevUrls) => ({
+      ...prevUrls,
+      [name]: value,
+    }));
+  }
+
+  // perform action with the URLS
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('URL 1:', urls.url1);
+    console.log('URL 2:', urls.url2);
+  }
+
   return (
     <div className='homepage'>
         <div className='navbar'> 
@@ -13,11 +37,32 @@ const HomePage = () => {
             <p> OpenAI will compare description information for these products and return a summary </p>
         </div>
 
-        <div className='links'>
-            <h2> Products to compare </h2>
-            <input name='link1' placeholder='Product URL 1'/>
-            <input name='link2' placeholder='Product URL 2'/>
-        </div>
+        <form onSubmit={handleSubmit} className='links'>
+          <div>
+            <input
+              type='url'
+              id='url1'
+              name='url1'
+              value={urls.url1}
+              onChange={handleChange}
+              placeholder='Product 1 URL'
+              required
+            />
+          </div>
+          
+          <div>
+            <input
+              type='url'
+              id='url2'
+              name='url2'
+              value={urls.url2}
+              onChange={handleChange}
+              placeholder='Product 2 URL'
+              required
+            />
+          </div>
+          <button type='submit'>Compare</button>
+        </form>
     </div>
 
 
