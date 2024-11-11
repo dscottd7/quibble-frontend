89b2f496e3c74af7a6d14f452fb66ee24b60a940
dscottdiperna <dscottdiperna@gmail.com>
2024-11-10 21:51:50 -0500

    switch progress bar to Mantine

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ProgressBar from '@ramonak/react-progress-bar';
+import { Progress } from '@mantine/core';
 
 const ProgressBarr = ({ value }) => {
   return (
     <div style={{ width: '100%', margin: '20px auto' }}>
-      <ProgressBar
-        completed={value}
-        bgColor="#007bff"
-        height="20px"
-        baseBgColor="#e0e0e0"
-        width="100%" 
-        isLabelVisible={true}
-        animateOnRender={true}
+      <Progress
+        value={value}
+        color="cyan"
       />
     </div>
   );
